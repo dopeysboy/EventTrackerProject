620fc2f7401e4677f720cb43be2309d360734901
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-11-20 12:19:14 -0600

    Finished GameService

--- ngEvents/src/app/services/game.service.ts
+++ ngEvents/src/app/services/game.service.ts
@@ -54,6 +54,50 @@ export class GameService {
     );
   }
 
+  getGameMultiplayer(isMp: boolean): Observable<Game[]>{
+    return this.http.get<Game[]>(`${this.url}/mp/${isMp}`).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.getGameMultiplayer(): error retrieving games')
+        )
+      })
+    );
+  }
+
+  getGamePublisher(publisher: string): Observable<Game[]>{
+    return this.http.get<Game[]>(`${this.url}/publisher/${publisher}`).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.getGamePublisher(): error retrieving games')
+        )
+      })
+    );
+  }
+
+  getGameKeyword(keyword: string): Observable<Game[]>{
+    return this.http.get<Game[]>(`${this.url}/keyword/${keyword}`).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.getGameKeyword(): error retrieving games')
+        )
+      })
+    );
+  }
+
+  getGamePriceRange(low: number, high: number): Observable<Game[]>{
+    return this.http.get<Game[]>(`${this.url}/msrp/${low}/${high}`).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.getGamePriceRange(): error retrieving games')
+        )
+      })
+    );
+  }
+
   update(gameId: number, game: Game): Observable<Game>{
     return this.http.put<Game>(`${this.url}/id/${gameId}`, game).pipe(
       catchError( (err: any) => {
@@ -87,6 +131,38 @@ export class GameService {
     );
   }
 
+  removePublisherFromGame(gameId: number, publisherId: number): Observable<Game>{
+    return this.http.put<Game>(`${this.url}/id/${gameId}/publisher/remove/${publisherId}`, {}).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.removePublisherFromGame(): error updating game')
+        )
+      })
+    );
+  }
+
+  addGenreToGame(gameId: number, genreId: number): Observable<Game>{
+    return this.http.put<Game>(`${this.url}/id/${gameId}/genre/add/${genreId}`, {}).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.addGenreToGame(): error updating game')
+        )
+      })
+    );
+  }
+
+  removeGenreFromGame(gameId: number, genreId: number): Observable<Game>{
+    return this.http.put<Game>(`${this.url}/id/${gameId}/genre/remove/${genreId}`, {}).pipe(
+      catchError( (err: any) => {
+        console.error(err);
+        return throwError(
+          () => new Error('GameService.removeGenreFromGame(): error updating game')
+        )
+      })
+    );
+  }
 
   constructor(private http: HttpClient) { }
 }
